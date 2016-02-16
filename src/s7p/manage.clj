6f379b500b939eddf7d6aff552d4a5b93399c821
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2016-02-16 19:22:06 +0900

    start writing manager

---
+++ src/s7p/manage.clj
@@ -0,0 +1,10 @@
+(ns s7p.manage
+  (:require [s7p.config :refer [advertisers dsps]]))
+
+
+(defn add-dsp [dsp]
+  (swap! dsps conj dsp))
+
+(defn remove-dsp [id]
+  (reset! dsps (remove #(= (:id _) id) @dsps)))
+
